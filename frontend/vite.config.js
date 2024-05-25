d8debadd2b86a0f0285b85469bd974c640ea729a
angelrivera7479 <angel.rivera7479@alumnos.udg.mx>
2024-05-25 16:38:14 -0600

    vite.config server-proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/socket.io": {
+        target: "https://proyecto-modular-server.vercel.app:4000",
+        ws: true,
+      },
+    },
+  },
+});
