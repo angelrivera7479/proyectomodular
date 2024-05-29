e7d52907801ab9d66a79f2e1e2d65505581f3537
angelrivera7479 <angel.rivera7479@alumnos.udg.mx>
2024-05-29 14:20:03 -0600

    Commit 1.1

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,33 @@
-import express from "express";
+import colors from "colors";
 import dotenv from "dotenv";
 dotenv.config();
+import express from "express";
 import http from "http";
 import { Server as SocketServer } from "socket.io";
-import colors from "colors";
 import cors from "cors";
-import sockets from "./sockets.js";
 
-//Conectar con base de datos
+//Conexión con base de datos
 import connectDB from "./config/db.js";
 connectDB();
 
 const app = express();
+
 app.use(cors());
+app.get("/", function (req, res) {
+  res.setHeader("Content-Security-Policy", "script-src 'self' 'unsafe-inline'");
+  res.send("<h1>BackEnd - Proyecto Modular 29/05/2024</h1>");
+  res.end();
+});
 
 const server = http.createServer(app);
-
 const io = new SocketServer(server, {
   cors: {
     origin: "https://proyecto-modular-client.vercel.app/",
   },
 });
 
-//Son los eventos del socket
-//Es donde esta el io.on("connection", (socket) => {}
+//Eventos de socket
+import sockets from "./sockets.js";
 sockets(io);
 
 const PORT = process.env.PORT;
