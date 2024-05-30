cdb366a5cd3798a2ada0ee25189c07b4c8340741
angelrivera7479 <angel.rivera7479@alumnos.udg.mx>
2024-05-29 18:50:39 -0600

    Update Cors 1.1

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.get("/", (req, res) => {
 const server = http.createServer(app);
 const io = new SocketServer(server, {
   cors: {
-    origin: "https://proyecto-modular-client.vercel.app/",
+    origin: "https://proyecto-modular-client.vercel.app/*",
   },
 });
 
